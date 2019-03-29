2f0124f3def1481d1ff5f047cb2c35808c57fb44
Lukas <s8lmscha@stud.uni-saarland.de>
2019-03-29 12:35:45 +0100

    Added interleaving, but it does not help much with burstpattern

--- mm_codecs/student.py
+++ mm_codecs/student.py
@@ -4,38 +4,60 @@ from typing import List, Generator
 def mm_encode(source: Generator[bytes, None, None]) -> Generator[bytes, None, None]:
  
     for byte in source:
-        # TODO: Do something sensible.
         work = int.from_bytes(byte, byteorder='big')       #give int representation of byte
-        #split in 4 bit front and 4 bit end (both represented in int)
         front_int = work >> 4
         end_int = work & 15
 
         front = ham74(front_int)
         end = ham74(end_int)
-      
-        # print("1: ", bytes(int(front,2)))
-        # print("2: ", int(front,2).to_bytes(1, byteorder='big', signed=False))
 
+        #interleaving
+        inter_table = [list(front[0:4]), list(front[4:]), list(end[:4]), list(end[4:])]
+        interleaved = []
+
+        for i in range(4):
+                for j in range(4):
+                        interleaved.append(inter_table[j][i])
+        
+        front_byte = int(''.join(interleaved[:8]), 2).to_bytes(1, byteorder='big', signed=False)
+        end_byte = int(''.join(interleaved[8:]), 2).to_bytes(1, byteorder='big', signed=False)
+      
         # one can only return ONE byte. So yield twice. YES, THIS WORKS AND NOBODY TOLD ME
-        yield int(front,2).to_bytes(1, byteorder='big', signed=False)
-        yield int(end, 2).to_bytes(1, byteorder='big', signed=False)
-        #print("orginal: ", byte)
+        # yield int(front,2).to_bytes(1, byteorder='big', signed=False)
+        # yield int(end, 2).to_bytes(1, byteorder='big', signed=False)
+
+        yield front_byte
+        yield end_byte
 
 
 def mm_decode(source: Generator[bytes, None, None]) -> Generator[bytes, None, None]:
     for byte in source:
 
-        # TODO: Do something sensible.
-        front_int = decode(byte)
-        end_int = decode(next(source))
-        # print("byte: ", byte, "  next: ", next(source))
+        # front_int = decode(byte)
+        # end_int = decode(next(source))
+        front_int = int.from_bytes(byte, byteorder='big')
+        end_int = int.from_bytes(next(source), byteorder='big')
+
+        front_list = list(bin(front_int)[2:].zfill(8))
+        end_list = list(bin(end_int)[2:].zfill(8))
+
+        inter_table = [front_list[:4], front_list[4:], end_list[:4], end_list[4:]]
+        dec_front, dec_end = [], []
+
+        for i in range(4):
+                for j in range(4):
+                        if i < 2:
+                                dec_front.append(inter_table[j][i])
+                        else:
+                                dec_end.append(inter_table[j][i])
+        
+        front_int = decode(int(''.join(dec_front), 2))
+        end_int = decode(int(''.join(dec_end), 2))
 
         #putting it back together
         ges = front_int << 4
         ges = ges + end_int
         erg = ges.to_bytes(1, byteorder='big', signed=False)
-        # print("rec: ", byte, " front: ", front_int, " end: ", end_int)
-        #print("rec: ", erg)
         yield erg
         
 
@@ -64,12 +86,10 @@ def originalBytes(x):
         binrep = bin(x)[2:].zfill(8)
         binx = binrep[3] + binrep[5:]
         binx = int(binx, 2)
-        #return binx.to_bytes(1, byteorder='big', signed=False)
         return binx
 
-def decode(byte):
-        x = int.from_bytes(byte, byteorder='big')
-        #print(bin(var[0])[2:].zfill(8))
+def decode(x):
+        #x = int.from_bytes(byte, byteorder='big')
         
         #Calculate s1, s2 and s3
         t1 = x & 0b01010101
@@ -82,7 +102,6 @@ def decode(byte):
         s3 = count1(t3) % 2
         if (s1+s2+s3) == 0:
                 # no error detected
-                #print("rec: ", originalBytes(x))
                 return originalBytes(x)
         else:
                 # Error detected
@@ -94,6 +113,4 @@ def decode(byte):
                 else:
                         s[location] = '1'
                 intrep = int(''.join(s), 2)
-                #print("byte: ", byte, "s1-s3", s1, s2, s3, "changed: ", ''.join(s), "  original data:  ", originalBytes(intrep))
-                #print("rec: ", originalBytes(intrep))
                 return originalBytes(intrep)
